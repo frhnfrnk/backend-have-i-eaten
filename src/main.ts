bb54f64fbc74104e4d74a3ab1e66ca9aeec45f60
Farhan Franaka <farhanfranaka0505@gmail.com>
2024-09-24 23:04:48 +0700

    fix error build

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
