08f4abd8c1c88f2273dfa5db18e20febb9f25347
Farhan Franaka <farhanfranaka0505@gmail.com>
2024-09-24 23:28:35 +0700

    fix error build

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3333);
 }
 bootstrap();
