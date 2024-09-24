169c675318279d79175b3abdbb473f5ab38c12e3
Farhan Franaka <farhanfranaka0505@gmail.com>
2024-09-24 22:40:57 +0700

    add cors and csrf

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import * as csurf from 'csurf';
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(csurf());
+
   await app.listen(3000);
 }
 bootstrap();
